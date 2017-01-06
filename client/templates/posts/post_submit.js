b849e37521b368354d41b5305ebfb53ed995dc69
Weston Cooper <weston@westoncooper.com>
2017-01-06 16:29:47 -0500

    enforce post URL uniqueness

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -8,8 +8,11 @@ Template.postSubmit.events({
         };
 
         Meteor.call('postInsert', post, function(error, result){
-            if(error)
+            if( error )
                 return alert(error.reason);
+
+            if ( result.postExists )
+                alert('This link has already been posted')
 
             Router.go('postPage', {_id: result._id});
         })
